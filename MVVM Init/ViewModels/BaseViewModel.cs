23b6e512e24634c73dd1b2eab6e6b584b70488af
DES-Destry <111andreyaa@gmail.com>
2020-03-18 19:01:09 +0600

    MVVM pattern implemented;  Little change in main window xaml code;

---
+++ MVVM Init/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM_Init.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
